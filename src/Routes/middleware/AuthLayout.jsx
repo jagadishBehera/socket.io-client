580e12d7d1bbf53b582b248a9bebae16f65ac5c9
Jagadish Behera <jagadishbehera961@gmail.com>
2026-03-30 17:24:23 +0530

    fix: removed unused variables1

--- src/Routes/middleware/AuthLayout.jsx
+++ src/Routes/middleware/AuthLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 
 const AuthLayout = (props) => {
   // if (!localStorage.getItem("encryptedData")) {
